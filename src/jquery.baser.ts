53b8b11e2d5c8dc19988678ed67b2b9491db4b18
Yusuke Hirao <yusukehirao@me.com>
2014-11-04 17:48:52 +0900

    $.fn.bcKeepAspectRatio追加

--- src/jquery.baser.ts
+++ src/jquery.baser.ts
@@ -175,6 +175,31 @@ $.fn.bcBackground = function (options: any): JQuery {
 	});
 };
 
+// クラスAPI化予定
+// since 0.0.9
+$.fn.bcKeepAspectRatio = function (): JQuery {
+	var $w: JQuery = $(window);
+
+	this.each( (i: number, elem: HTMLElement): void => {
+		var $elem: JQuery = $(elem);
+		var baseWidth: number = <number> +$elem.data('width');
+		var baseHeight: number = <number> +$elem.data('height');
+		var aspectRatio: number = baseWidth / baseHeight;
+		$w.on('resize', (): void => {
+			var width: number = $elem.width();
+			$elem.css({
+				width: '100%',
+				height: width / aspectRatio
+			});
+		}).trigger('resize');
+	});
+
+	baser.ui.Timer.wait(30, () => {
+		$w.trigger('resize');
+	});
+	return this;
+};
+
 // since 0.0.8
 $.fn.bcScrollTo = function (options?: baser.ui.ScrollOptions): JQuery {
 	return this.on('click', function (e: JQueryMouseEventObject): void {
