f906794fea56e35d794de7c0efb08fbaab49e62b
Yusuke Hirao <yusukehirao@me.com>
2014-11-18 04:39:46 +0900

    baser.utility.String にクラス名関連の関数を追加

--- src/baser/utility/String.ts
+++ src/baser/utility/String.ts
@@ -33,6 +33,67 @@ module baser {
 
 			}
 
+			/**
+			 * ハイフン チェインケース化
+			 *
+			 * @version 0.1.0
+			 * @since 0.1.0
+			 *
+			 */
+			static hyphenDelimited (str: string): string {
+
+				var words: string[] = str.replace(/[A-Z]/g, ($1: string): string => {
+					return ' ' + $1.toLowerCase();
+				}).split(/[^a-z0-9]+/ig);
+
+				var result: string[] = [];
+
+				var i: number = 0;
+				var l: number = words.length;
+				for (; i < l; i++) {
+					if (words[i]) {
+						result.push(words[i].toLowerCase());
+					}
+				}
+
+				return result.join('-');
+
+			}
+
+			/**
+			 * スネークケース化
+			 *
+			 * @version 0.1.0
+			 * @since 0.1.0
+			 *
+			 */
+			static snakeCase (str: string): string {
+
+				return String.hyphenDelimited(str).replace(/-/g, '_');
+
+			}
+
+			/**
+			 * キャメルケース化
+			 *
+			 * @version 0.1.0
+			 * @since 0.1.0
+			 *
+			 */
+			static camelCase (str: string, upperCase: boolean = false): string {
+
+				var hdStr: string = String.hyphenDelimited(str);
+
+				if (upperCase && /^[a-z]/.test(hdStr)) {
+					hdStr = '-' + hdStr;
+				}
+
+				return hdStr.replace(/-([a-z])/g, ($1: string, $2: string): string => {
+					return $2.toUpperCase();
+				});
+
+			}
+
 		}
 
 	}
