746603a3893efa1eccbca0479d1610d61f5aeb3f
Yusuke Hirao <yusukehirao@me.com>
2015-04-11 03:40:52 +0900

    TextFieldクラス追加 IE8のplaceholder対応

---
+++ src/baser/ui/element/ITextField.ts
@@ -0,0 +1,19 @@
+module baser {
+
+	export module ui {
+
+		export module element {
+
+			export interface ITextField extends IFormElement {
+
+				isEmpty: boolean;
+				placeholder: string;
+				hasPlaceholder: boolean;
+
+			}
+
+		}
+
+	}
+
+}
