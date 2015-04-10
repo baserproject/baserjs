d8704e0b62691422483473a99ab4ec0ba5f66d2c
Yusuke Hirao <yusukehirao@me.com>
2015-04-11 04:02:01 +0900

    fixed #13 #12 FormElement / Select / TextField API改善

--- baser.d.ts
+++ baser.d.ts
@@ -1151,7 +1151,7 @@ declare module baser {
                  * @since 0.4.0
                  *
                  */
-                setValue(value: string | number | boolean): void;
+                setValue(value: string | number | boolean, isSilent?: boolean): void;
                 /**
                  * 無効状態を設定する
                  *
@@ -1160,6 +1160,182 @@ declare module baser {
                  *
                  */
                 setDisabled(isDisabled: boolean): void;
+            }
+        }
+    }
+}
+declare module baser {
+    module ui {
+        module element {
+            interface ITextField extends IFormElement {
+                isEmpty: boolean;
+                placeholder: string;
+                hasPlaceholder: boolean;
+            }
+        }
+    }
+}
+declare module baser {
+    module ui {
+        module element {
+            /**
+             * TextFieldクラスのオプションハッシュのインターフェイス
+             *
+             * @version 0.4.0
+             * @since 0.4.0
+             *
+             */
+            interface TextFieldOption extends FormElementOption {
+            }
+            /**
+             * テキストフィールドの拡張クラス
+             *
+             * @version 0.4.0
+             * @since 0.4.0
+             *
+             */
+            class TextField extends FormElement implements ITextField {
+                /**
+                 * オプションのデフォルト値
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                static defaultOption: TextFieldOption;
+                /**
+                 * TextField要素のクラス
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                static classNameTextField: string;
+                /**
+                 * 未入力状態に付加されるクラス
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                static classNameStateUninput: string;
+                /**
+                 * プレースホルダー属性に対応しているかどうか
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                static supportPlaceholder: boolean;
+                /**
+                 * 空かどうか
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                isEmpty: boolean;
+                /**
+                 * プレースホルダーテキスト
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                placeholder: string;
+                /**
+                 * プレースホルダーをもっているかどうか
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                hasPlaceholder: boolean;
+                /**
+                 * コンストラクタ
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 * @param $el 管理するDOM要素のjQueryオブジェクト
+                 * @param options オプション
+                 *
+                 */
+                constructor($el: JQuery, options: TextFieldOption);
+                /**
+                 * クラス名を設定する
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 * @override
+                 *
+                 */
+                protected _setClassName(): void;
+                /**
+                 * ラップ要素を生成
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 * @override
+                 *
+                 */
+                protected _createWrapper(): void;
+                /**
+                 * イベントの登録
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 * @override
+                 *
+                 */
+                protected _bindEvents(config: TextFieldOption): void;
+                /**
+                 * 要素の状態を更新する
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                private _update();
+                /**
+                 * 入力されている状態を設定する
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                private _setStateInputted();
+                /**
+                 * 入力されていない状態を設定する
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                private _setStateUninputted();
+                /**
+                 * プレースホルダーと値が同じかどうか
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                private _equalPlaceholder();
+                /**
+                 * プレースホルダーの値を設定する
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                private _setPlaceholderValue();
+                /**
+                 * 【IE用】カーソル（キャレット）を先頭に持っていく
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                private _msCaretMoveToTop();
             }
         }
     }
--- baser.js
+++ baser.js
@@ -1,5 +1,5 @@
 /**
- * baserjs - v0.4.0-beta r218
+ * baserjs - v0.4.0 r220
  * update: 2015-04-11
  * Author: baserCMS Users Community [https://github.com/baserproject/]
  * Github: https://github.com/baserproject/baserjs
@@ -2241,12 +2241,13 @@ var baser;
                  * @since 0.4.0
                  *
                  */
-                FormElement.prototype.setValue = function (value) {
+                FormElement.prototype.setValue = function (value, isSilent) {
+                    if (isSilent === void 0) { isSilent = false; }
                     var valueString = String(value);
                     var currentValue = this.$el.val();
                     if (currentValue !== valueString) {
                         this.$el.val(valueString);
-                        this._fireChangeEvent();
+                        this._fireChangeEvent(isSilent);
                     }
                 };
                 /**
@@ -2344,6 +2345,242 @@ var baser;
         var element;
         (function (element) {
             /**
+             * テキストフィールドの拡張クラス
+             *
+             * @version 0.4.0
+             * @since 0.4.0
+             *
+             */
+            var TextField = (function (_super) {
+                __extends(TextField, _super);
+                /**
+                 * コンストラクタ
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 * @param $el 管理するDOM要素のjQueryオブジェクト
+                 * @param options オプション
+                 *
+                 */
+                function TextField($el, options) {
+                    _super.call(this, $el, options);
+                    /**
+                     * プレースホルダーテキスト
+                     *
+                     * @version 0.4.0
+                     * @since 0.4.0
+                     *
+                     */
+                    this.placeholder = '';
+                    this.placeholder = this.$el.attr('placeholder') || '';
+                    this.hasPlaceholder = !!this.placeholder;
+                    this._update();
+                }
+                /**
+                 * クラス名を設定する
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 * @override
+                 *
+                 */
+                TextField.prototype._setClassName = function () {
+                    _super.prototype._setClassName.call(this);
+                    // セレクトボックス用のクラス名を設定
+                    this.addClass(TextField.classNameTextField);
+                };
+                /**
+                 * ラップ要素を生成
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 * @override
+                 *
+                 */
+                TextField.prototype._createWrapper = function () {
+                    _super.prototype._createWrapper.call(this);
+                    element.Element.addClassTo(this.$wrapper, TextField.classNameTextField + '-' + element.FormElement.classNameWrapper);
+                };
+                /**
+                 * イベントの登録
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 * @override
+                 *
+                 */
+                TextField.prototype._bindEvents = function (config) {
+                    var _this = this;
+                    _super.prototype._bindEvents.call(this, config);
+                    // keyupイベントが起こった場合に実行するルーチン
+                    $(document).on('keyup.bcTextField-' + this.id, function (e) {
+                        if (_this.hasFocus) {
+                            _this._update();
+                        }
+                    });
+                    // プレースホルダーをサポートしていない時のイベント処理
+                    if (!TextField.supportPlaceholder) {
+                        // フォーカスを当てた時・クリックをしたときにプレースホルダーと値が同じだった場合
+                        // カーソル（キャレット）を先頭に持っていく
+                        this.$el.on('focus.bcTextField click.bcTextField', function () {
+                            if (_this._equalPlaceholder()) {
+                                _this._msCaretMoveToTop();
+                            }
+                        });
+                        // キーボードを押した瞬間に、プレースホルダーと値が同じだった場合
+                        // プレースホルダーの値を消して、空にする
+                        // TODO: 文字以外のキーを押すと一瞬値が消える（クリティカルでないため保留）
+                        $(document).on('keydown.bcTextField-' + this.id, function (e) {
+                            if (_this.hasFocus) {
+                                if (_this._equalPlaceholder()) {
+                                    _this.$el.val('');
+                                }
+                            }
+                        });
+                    }
+                };
+                /**
+                 * 要素の状態を更新する
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                TextField.prototype._update = function () {
+                    var currentValue = this.$el.val() || '';
+                    var isEmpty = !currentValue;
+                    if (TextField.supportPlaceholder) {
+                        if (isEmpty) {
+                            this._setStateUninputted();
+                        }
+                        else {
+                            this._setStateInputted();
+                        }
+                    }
+                    else {
+                        if (this._equalPlaceholder()) {
+                            this._setStateUninputted();
+                        }
+                        else {
+                            if (isEmpty) {
+                                this._setStateUninputted();
+                                this._setPlaceholderValue();
+                            }
+                            else {
+                                this._setStateInputted();
+                            }
+                        }
+                    }
+                };
+                /**
+                 * 入力されている状態を設定する
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                TextField.prototype._setStateInputted = function () {
+                    this.isEmpty = false;
+                    element.Element.removeClassFrom(this.$el, element.FormElement.classNameFormElementCommon, '', TextField.classNameStateUninput);
+                    element.Element.removeClassFrom(this.$label, element.FormElement.classNameFormElementCommon, element.FormElement.classNameLabel, TextField.classNameStateUninput);
+                    element.Element.removeClassFrom(this.$wrapper, element.FormElement.classNameWrapper, '', TextField.classNameStateUninput);
+                };
+                /**
+                 * 入力されていない状態を設定する
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                TextField.prototype._setStateUninputted = function () {
+                    this.isEmpty = true;
+                    element.Element.addClassTo(this.$el, element.FormElement.classNameFormElementCommon, '', TextField.classNameStateUninput);
+                    element.Element.addClassTo(this.$label, element.FormElement.classNameFormElementCommon, element.FormElement.classNameLabel, TextField.classNameStateUninput);
+                    element.Element.addClassTo(this.$wrapper, element.FormElement.classNameWrapper, '', TextField.classNameStateUninput);
+                };
+                /**
+                 * プレースホルダーと値が同じかどうか
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                TextField.prototype._equalPlaceholder = function () {
+                    var currentValue = this.$el.val() || '';
+                    return this.placeholder === currentValue;
+                };
+                /**
+                 * プレースホルダーの値を設定する
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                TextField.prototype._setPlaceholderValue = function () {
+                    this.$el.val(this.placeholder);
+                    this._msCaretMoveToTop();
+                };
+                /**
+                 * 【IE用】カーソル（キャレット）を先頭に持っていく
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                TextField.prototype._msCaretMoveToTop = function () {
+                    // TODO: MS用の型を調査して定義
+                    var input = this.$el[0];
+                    var range = input.createTextRange();
+                    range.collapse();
+                    range.moveStart('character', 0);
+                    range.moveEnd('character', 0);
+                    range.select();
+                };
+                /**
+                 * オプションのデフォルト値
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                TextField.defaultOption = {};
+                /**
+                 * TextField要素のクラス
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                TextField.classNameTextField = 'form-text-field';
+                /**
+                 * 未入力状態に付加されるクラス
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                TextField.classNameStateUninput = 'uninput';
+                /**
+                 * プレースホルダー属性に対応しているかどうか
+                 *
+                 * @version 0.4.0
+                 * @since 0.4.0
+                 *
+                 */
+                TextField.supportPlaceholder = $('<input />').prop('placeholder') !== undefined;
+                return TextField;
+            })(element.FormElement);
+            element.TextField = TextField;
+        })(element = ui.element || (ui.element = {}));
+    })(ui = baser.ui || (baser.ui = {}));
+})(baser || (baser = {}));
+var baser;
+(function (baser) {
+    var ui;
+    (function (ui) {
+        var element;
+        (function (element) {
+            /**
              * セレクトボックスの拡張クラス
              *
              * @version 0.1.0
@@ -2362,8 +2599,7 @@ var baser;
                  *
                  */
                 function Select($el, options) {
-                    var config = $.extend({}, element.FormElement.defaultOption, Select.defaultOption, options);
-                    _super.call(this, $el, config);
+                    _super.call(this, $el, options);
                     this._update();
                 }
                 /**
@@ -4403,6 +4639,8 @@ var baser;
 /// <reference path="baser/ui/element/Form.ts" />
 /// <reference path="baser/ui/element/IFormElement.ts" />
 /// <reference path="baser/ui/element/FormElement.ts" />
+/// <reference path="baser/ui/element/ITextField.ts" />
+/// <reference path="baser/ui/element/TextField.ts" />
 /// <reference path="baser/ui/element/ISelect.ts" />
 /// <reference path="baser/ui/element/Select.ts" />
 /// <reference path="baser/ui/element/ICheckableElement.ts" />
