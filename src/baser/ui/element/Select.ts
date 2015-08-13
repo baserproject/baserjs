61f9576245cdae3cedd93ee0f352397d9b499500
Yusuke Hirao <yusukehirao@me.com>
2015-08-13 13:14:17 +0900

    Select APIのoption要素のdisabledに対応

--- src/baser/ui/element/Select.ts
+++ src/baser/ui/element/Select.ts
@@ -546,20 +546,28 @@ module baser.ui.element {
 			this.$el.find('option').each( (i: number, opt: HTMLElement): void => {
 				var $opt: JQuery = $(opt);
 				var isSelected: boolean;
+				var isDisabled: boolean;
 				var $psuedoOpt: JQuery;
 				isSelected = <boolean> $opt.prop('selected');
+				isDisabled = <boolean> $opt.prop('disabled');
 				if (isSelected) {
 					this.$selected.text($opt.text());
 				}
 				if (this.$options) {
 					$psuedoOpt = $psuedoOptList.eq(i);
 					$psuedoOpt.attr('aria-selected', <string> '' + isSelected);
+					$psuedoOpt.attr('aria-disabled', <string> '' + isDisabled);
 					if (isSelected) {
 						Element.addClassTo($psuedoOpt, Select.classNameSelectOptionList, Select.classNameSelectOption, Select.classNameStateSelected);
 						Element.removeClassFrom($psuedoOpt, Select.classNameSelectOptionList, Select.classNameSelectOption, Select.classNameStateUnselected);
 					} else {
 						Element.addClassTo($psuedoOpt, Select.classNameSelectOptionList, Select.classNameSelectOption, Select.classNameStateUnselected);
 						Element.removeClassFrom($psuedoOpt, Select.classNameSelectOptionList, Select.classNameSelectOption, Select.classNameStateSelected);
+					}
+					if (isDisabled) {
+						Element.addClassTo($psuedoOpt, Select.classNameSelectOptionList, Select.classNameSelectOption, Select.classNameStateDisabled);
+					} else {
+						Element.removeClassFrom($psuedoOpt, Select.classNameSelectOptionList, Select.classNameSelectOption, Select.classNameStateDisabled);
 					}
 				}
 			});
@@ -586,13 +594,13 @@ module baser.ui.element {
 		/**
 		 * 値をインデックス番号から設定する
 		 *
-		 * @version 0.4.1
+		 * @version 0.8.0
 		 * @since 0.4.0
 		 *
 		 */
 		public setIndex (index: number, isSilent: boolean = false): void {
 			var $targetOption: JQuery = this.$el.find('option').eq(index);
-			if ($targetOption.length && !$targetOption.prop('selected')) {
+			if ($targetOption.length && !$targetOption.prop('selected') && !$targetOption.prop('disabled')) {
 				$targetOption.prop('selected', true);
 				this._fireChangeEvent(isSilent);
 			}
