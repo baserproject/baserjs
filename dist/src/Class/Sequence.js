53ccb0d3bd4ae737d3a7ca40a2f7476fd8e598db
Yusuke Hirao <yusukehirao@me.com>
2015-09-05 10:06:53 +0900

    リファクタリング

--- dist/src/Class/Sequence.js
+++ dist/src/Class/Sequence.js
@@ -1,120 +1,302 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var EventDispatcher = require('./EventDispatcher');
+var Timer = require('./Timer');
 /**
  * 非同期逐次処理クラス
  *
- * @version 0.4.0
+ * @version 0.9.0
  * @since 0.4.0
  *
  */
-var Sequence = (function () {
+var Sequence = (function (_super) {
+    __extends(Sequence, _super);
+    /**
+     * コンストラクタ
+     *
+     * @version 0.9.0
+     * @since 0.4.0
+     * @param tasks タスク
+     *
+     */
     function Sequence(tasks) {
+        _super.call(this);
+        /**
+         * シーケンスの持つタスク
+         *
+         * @version 0.4.0
+         * @since 0.4.0
+         *
+         */
         this._tasks = [];
-        this._index = 0;
+        /**
+         * 現在実行中のタスク番号
+         *
+         * @version 0.9.0
+         * @since 0.4.0
+         *
+         */
+        this._currentTaskIndex = 0;
+        /**
+         * タスクを実行したトータルカウント数
+         *
+         * @version 0.4.0
+         * @since 0.4.0
+         *
+         */
         this._iterator = 0;
+        /**
+         * シーケンスのプロミスオブジェクト
+         *
+         * @version 0.4.0
+         * @since 0.4.0
+         *
+         */
         this._promise = null;
+        /**
+         * シーケンスのリゾルバ
+         *
+         * @version 0.4.0
+         * @since 0.4.0
+         *
+         */
         this._resolver = null;
+        /**
+         * 遅延時間
+         *
+         * @version 0.4.0
+         * @since 0.4.0
+         *
+         */
         this._waitingTime = 0;
-        this._waitTimer = 0;
-        this._toExit = false;
-        var i = 0;
-        var l = tasks.length;
-        for (; i < l; i++) {
-            this._tasks.push(new Task(tasks[i]));
+        /**
+         * 遅延用タイマー
+         *
+         * @version 0.9.0
+         * @since 0.4.0
+         *
+         */
+        this._waitTimer = new Timer();
+        /**
+         * 停止状態
+         *
+         * @version 0.4.0
+         * @since 0.4.0
+         *
+         */
+        this._isStop = true;
+        for (var i = 0, l = tasks.length; i < l; i++) {
+            this._tasks.push(new Task(tasks[i], this));
         }
     }
-    // TODO: ネイティブのPromiseを使う
+    /**
+     * タスクの実行
+     * TODO: ネイティブのPromiseを使う
+     *
+     * @version 0.9.0
+     * @since 0.4.0
+     * @param value タスクに渡すデータ
+     * @param isLoop 最後のタスクが終了したあとに最初に戻ってループ処理をするかどうか
+     * @return インスタンス自身
+     *
+     */
     Sequence.prototype.act = function (value, isLoop) {
         var _this = this;
         if (isLoop === void 0) { isLoop = false; }
-        var task = this._tasks[this._index];
-        var result = task.act(this, this._iterator, value);
-        // Type like JQueryDeferred
-        if (isJQueryPromiseLikeObject(result)) {
-            this._promise = result.promise();
-        }
-        else {
-            this._resolver = $.Deferred();
-            this._waitTimer = setTimeout(function () {
-                _this._resolver.resolve(result);
-            }, this._waitingTime);
-            // promised
-            this._promise = this._resolver.promise();
-        }
+        // ストップ状態解除
+        this._isStop = false;
+        // TODO: 引数の設計とテスト書く
+        this.trigger('beforeact');
+        // タスク取得
+        var task = this._tasks[this._currentTaskIndex];
+        // タスク実行
+        var result = task.act(value);
+        // 戻り値によるプロミスの設定
+        this._setPromiseFrom(result);
+        // プロミスの結果から次のタスクを実行
         this._promise.done(function (doneResult) {
-            clearTimeout(_this._waitTimer);
-            _this._promise = null;
-            _this._resolver = null;
-            _this._waitTimer = null;
-            _this._waitingTime = 0;
-            _this._index += 1;
+            _this._reset();
+            _this._currentTaskIndex += 1;
             _this._iterator += 1;
-            if (!_this._toExit && (_this._index < _this._tasks.length || isLoop)) {
-                if (_this._index >= _this._tasks.length && isLoop) {
-                    _this._index = 0;
+            if (!_this._isStop && (_this._currentTaskIndex < _this._tasks.length || isLoop)) {
+                if (_this._currentTaskIndex >= _this._tasks.length && isLoop) {
+                    _this._currentTaskIndex = 0;
                 }
                 _this.act(doneResult, isLoop);
             }
+            else {
+                // TODO: 引数の設計とテスト書く
+                _this.trigger('stop');
+            }
         }).fail(function () {
-            clearTimeout(_this._waitTimer);
-            _this._promise = null;
-            _this._resolver = null;
-            _this._waitTimer = null;
-            _this._waitingTime = 0;
+            _this._reset();
+            // TODO: 引数の設計とテスト書く
+            _this.trigger('exit');
+            _this.trigger('stop');
         });
         return this;
     };
+    /**
+     * プロミスの設定
+     * TODO: ネイティブのPromiseを使う
+     * TODO: this._waitTimerのTimerクラスにcancelイベントを実装してリゾルバのリジェクトを実装する
+     *
+     * @version 0.9.0
+     * @since 0.9.0
+     *
+     */
+    Sequence.prototype._setPromiseFrom = function (value) {
+        var _this = this;
+        if (this._isJQueryPromiseLikeObject(value)) {
+            // 値がプロミスであればそのままそれを設定
+            this._promise = value.promise();
+        }
+        else {
+            // 値がプロミスでない場合は
+            // プロミスを生成してリゾルバへ一時的に設定
+            this._resolver = $.Deferred();
+            this._promise = this._resolver.promise();
+            // タイマーを使い遅延実行後リゾルバからプロミスを解決
+            this._waitTimer.wait(this._waitingTime, function () {
+                _this._resolver.resolve(value);
+            });
+            // TODO: Timerクラス側が未実装
+            this._waitTimer.on('cencel', function () {
+                _this._resolver.reject(value);
+            });
+        }
+    };
+    /**
+     * 次のタスクを実行するために一時的なオブジェクトをリセットする
+     *
+     * @version 0.9.0
+     * @since 0.9.0
+     *
+     */
+    Sequence.prototype._reset = function () {
+        this._waitTimer.stop();
+        this._promise = null;
+        this._resolver = null;
+        this._waitingTime = 0;
+    };
+    /**
+     * jQuery Promiseに近いオブジェクト化どうかを判定する
+     *
+     * @version 0.9.0
+     * @since 0.4.0
+     * @param object 対象のオブジェクト
+     * @return 判定結果
+     *
+     */
+    Sequence.prototype._isJQueryPromiseLikeObject = function (object) {
+        // 以下列挙したプロパティのみをメンバにもち、かつ全て関数オブジェクトであること
+        var PROPS = [
+            'always',
+            'done',
+            'fail',
+            'pipe',
+            'progress',
+            'promise',
+            'state',
+            'then'
+        ];
+        if (object instanceof jQuery) {
+            return !!object.promise;
+        }
+        else {
+            object = Object(object);
+            while (PROPS.length) {
+                var propsName = PROPS.shift();
+                if (!(propsName in object && $.isFunction(object[propsName]))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    };
+    /**
+     * ループでタスクの実行
+     *
+     * @version 0.4.0
+     * @since 0.4.0
+     *
+     */
     Sequence.prototype.loop = function (value) {
         return this.act(value, true);
     };
+    /**
+     * シーケンス処理から抜ける
+     *
+     * @version 0.9.0
+     * @since 0.4.0
+     *
+     */
     Sequence.prototype.exit = function () {
-        this._toExit = true;
+        this._isStop = true;
         if (this._resolver) {
             this._resolver.reject();
         }
         return this;
     };
+    /**
+    * ___
+    *
+    * @version 0.4.0
+    * @since 0.4.0
+    *
+    */
     Sequence.prototype.wait = function (watingTime) {
         this._waitingTime = watingTime;
     };
+    /**
+     * タスクを実行したトータルカウント数を取得
+     *
+     * @version 0.9.0
+     * @since 0.9.0
+     *
+     */
+    Sequence.prototype.getCount = function () {
+        return this._iterator;
+    };
     return Sequence;
-})();
+})(EventDispatcher);
+/**
+ * タスクのクラス
+ *
+ * @private
+ * @version 0.9.0
+ * @since 0.4.0
+ *
+ */
 var Task = (function () {
-    function Task(func) {
-        this.status = TaskState.yet;
+    /**
+     * コンストラクタ
+     *
+     * @version 0.9.0
+     * @since 0.4.0
+     *
+     */
+    function Task(func, sequencer) {
         this._func = func;
+        this._sequencer = sequencer;
     }
-    Task.prototype.act = function (sequence, sequenceIndex, value) {
-        var result = this._func.call(sequence, sequenceIndex, value);
-        this.status = TaskState.done;
+    /**
+     * タスクの実行
+     *
+     * @version 0.9.0
+     * @since 0.4.0
+     * @param value タスクに渡すデータ
+     * @return 実行したタスクの戻り値
+     *
+     */
+    Task.prototype.act = function (value) {
+        var result = this._func.call(this._sequencer, this._sequencer.getCount(), value);
         return result;
     };
     return Task;
 })();
-var TaskState;
-(function (TaskState) {
-    TaskState[TaskState["done"] = 0] = "done";
-    TaskState[TaskState["yet"] = 1] = "yet";
-})(TaskState || (TaskState = {}));
-function isJQueryPromiseLikeObject(object) {
-    var props = [
-        'always',
-        'done',
-        'fail',
-        'pipe',
-        'progress',
-        'promise',
-        'state',
-        'then'
-    ];
-    if (object instanceof jQuery) {
-        return !!object.promise;
-    }
-    else {
-        while (props.length) {
-            if (!(props.shift() in Object(object))) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
 module.exports = Sequence;
--- src/Class/Sequence.ts
+++ src/Class/Sequence.ts
@@ -1,132 +1,338 @@
+import DispatchEvent = require('./DispatchEvent');
+import EventDispatcher = require('./EventDispatcher');
+import Timer = require('./Timer');
+
 /**
  * 非同期逐次処理クラス
  *
- * @version 0.4.0
+ * @version 0.9.0
  * @since 0.4.0
  *
  */
-class Sequence {
+class Sequence extends EventDispatcher {
 
+	/**
+	 * シーケンスの持つタスク
+	 *
+	 * @version 0.4.0
+	 * @since 0.4.0
+	 *
+	 */
 	private _tasks: Task[] = [];
-	private _index: number = 0;
+	
+	/**
+	 * 現在実行中のタスク番号
+	 *
+	 * @version 0.9.0
+	 * @since 0.4.0
+	 *
+	 */
+	private _currentTaskIndex: number = 0;
+	
+	/**
+	 * タスクを実行したトータルカウント数
+	 *
+	 * @version 0.4.0
+	 * @since 0.4.0
+	 *
+	 */
 	private _iterator: number = 0;
+	
+	/**
+	 * シーケンスのプロミスオブジェクト
+	 *
+	 * @version 0.4.0
+	 * @since 0.4.0
+	 *
+	 */
 	private _promise: JQueryPromise<any> = null;
+	
+	/**
+	 * シーケンスのリゾルバ
+	 *
+	 * @version 0.4.0
+	 * @since 0.4.0
+	 *
+	 */
 	private _resolver: JQueryDeferred<any> = null;
+	
+	/**
+	 * 遅延時間
+	 *
+	 * @version 0.4.0
+	 * @since 0.4.0
+	 *
+	 */
 	private _waitingTime: number = 0;
-	private _waitTimer: number = 0;
-	private _toExit: boolean = false;
+	
+	/**
+	 * 遅延用タイマー
+	 *
+	 * @version 0.9.0
+	 * @since 0.4.0
+	 *
+	 */
+	private _waitTimer: Timer = new Timer();
+	
+	/**
+	 * 停止状態
+	 *
+	 * @version 0.4.0
+	 * @since 0.4.0
+	 *
+	 */
+	private _isStop: boolean = true;
 
+	/**
+	 * コンストラクタ
+	 *
+	 * @version 0.9.0
+	 * @since 0.4.0
+	 * @param tasks タスク
+	 *
+	 */
 	constructor (tasks: Function[]) {
-		var i: number = 0;
-		var l: number = tasks.length;
-		for (; i < l; i++) {
-			this._tasks.push(new Task(tasks[i]));
+		super();
+		for (let i: number = 0, l: number = tasks.length; i < l; i++) {
+			this._tasks.push(new Task(tasks[i], this));
 		}
 	}
 
-	// TODO: ネイティブのPromiseを使う
+	/**
+	 * タスクの実行
+	 * TODO: ネイティブのPromiseを使う
+	 *
+	 * @version 0.9.0
+	 * @since 0.4.0
+	 * @param value タスクに渡すデータ
+	 * @param isLoop 最後のタスクが終了したあとに最初に戻ってループ処理をするかどうか
+	 * @return インスタンス自身
+	 *
+	 */
 	public act (value: any, isLoop: boolean = false): Sequence {
-		var task: Task = this._tasks[this._index];
-		var result: any = task.act(this, this._iterator, value);
 
-		// Type like JQueryDeferred
-		if (isJQueryPromiseLikeObject(result)) {
-			this._promise = result.promise();
-		// Type Another
-		} else {
-			this._resolver = $.Deferred<any>();
-			this._waitTimer = setTimeout( (): void => {
-				this._resolver.resolve(result);
-			}, this._waitingTime);
-			// promised
-			this._promise = this._resolver.promise();
-		}
+		// ストップ状態解除
+		this._isStop = false;
+
+		// TODO: 引数の設計とテスト書く
+		this.trigger('beforeact');
+
+		// タスク取得
+		let task: Task = this._tasks[this._currentTaskIndex];
+		
+		// タスク実行
+		let result: any = task.act(value);
+
+		// 戻り値によるプロミスの設定
+		this._setPromiseFrom(result);
+		
+		// プロミスの結果から次のタスクを実行
 		this._promise.done( (doneResult: any): void => {
-			clearTimeout(this._waitTimer);
-			this._promise = null;
-			this._resolver = null;
-			this._waitTimer = null;
-			this._waitingTime = 0;
-			this._index += 1;
+			this._reset();
+			this._currentTaskIndex += 1;
 			this._iterator += 1;
-			if (!this._toExit && (this._index < this._tasks.length || isLoop)) {
-				if (this._index >= this._tasks.length && isLoop) {
-					this._index = 0;
+			if (!this._isStop && (this._currentTaskIndex < this._tasks.length || isLoop)) {
+				if (this._currentTaskIndex >= this._tasks.length && isLoop) {
+					this._currentTaskIndex = 0;
 				}
 				this.act(doneResult, isLoop);
+			} else {
+				// TODO: 引数の設計とテスト書く
+				this.trigger('stop');
 			}
 		}).fail( (): void => {
-			clearTimeout(this._waitTimer);
-			this._promise = null;
-			this._resolver = null;
-			this._waitTimer = null;
-			this._waitingTime = 0;
+			this._reset();
+			// TODO: 引数の設計とテスト書く
+			this.trigger('exit');
+			this.trigger('stop');
 		});
 		return this;
 	}
 
+	/**
+	 * プロミスの設定
+	 * TODO: ネイティブのPromiseを使う
+	 * TODO: this._waitTimerのTimerクラスにcancelイベントを実装してリゾルバのリジェクトを実装する
+	 *
+	 * @version 0.9.0
+	 * @since 0.9.0
+	 *
+	 */
+	private _setPromiseFrom (value: any): void {
+		if (this._isJQueryPromiseLikeObject(value)) {
+			// 値がプロミスであればそのままそれを設定
+			this._promise = value.promise();
+		} else {
+			// 値がプロミスでない場合は
+			// プロミスを生成してリゾルバへ一時的に設定
+			this._resolver = $.Deferred<any>();
+			this._promise = this._resolver.promise();
+			// タイマーを使い遅延実行後リゾルバからプロミスを解決
+			this._waitTimer.wait(this._waitingTime, (): void => {
+				this._resolver.resolve(value);
+			});
+			// TODO: Timerクラス側が未実装
+			this._waitTimer.on('cencel', () => {
+				this._resolver.reject(value);
+			});
+		}
+	}
+	
+	/**
+	 * 次のタスクを実行するために一時的なオブジェクトをリセットする
+	 *
+	 * @version 0.9.0
+	 * @since 0.9.0
+	 *
+	 */
+	private _reset (): void {
+		this._waitTimer.stop();
+		this._promise = null;
+		this._resolver = null;
+		this._waitingTime = 0;
+	}
+
+	/**
+	 * jQuery Promiseに近いオブジェクト化どうかを判定する
+	 *
+	 * @version 0.9.0
+	 * @since 0.4.0
+	 * @param object 対象のオブジェクト
+	 * @return 判定結果
+	 *
+	 */
+	private _isJQueryPromiseLikeObject (object: any): boolean {
+		// 以下列挙したプロパティのみをメンバにもち、かつ全て関数オブジェクトであること
+		const PROPS: string[] = [
+			'always',
+			'done',
+			'fail',
+			'pipe',
+			'progress',
+			'promise',
+			'state',
+			'then'
+		];
+		if (object instanceof jQuery) {
+			return !!object.promise;
+		} else {
+			object = Object(object);
+			while (PROPS.length) {
+				let propsName: string = PROPS.shift();
+				if (!(propsName in object && $.isFunction(object[propsName]))) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+
+	/**
+	 * ループでタスクの実行
+	 *
+	 * @version 0.4.0
+	 * @since 0.4.0
+	 *
+	 */
 	public loop (value: any): Sequence {
 		return this.act(value, true);
 	}
 
+	/**
+	 * シーケンス処理から抜ける
+	 *
+	 * @version 0.9.0
+	 * @since 0.4.0
+	 *
+	 */
 	public exit (): Sequence {
-		this._toExit = true;
+		this._isStop = true;
 		if (this._resolver) {
 			this._resolver.reject();
 		}
 		return this;
 	}
 
+	/**
+	* ___
+	*
+	* @version 0.4.0
+	* @since 0.4.0
+	*
+	*/
 	public wait (watingTime: number): void {
 		this._waitingTime = watingTime;
 	}
 
-}
-
-class Task {
-
-	public status: TaskState;
-	private _func: Function;
-
-	constructor (func: Function) {
-		this.status = TaskState.yet;
-		this._func = func;
+	/**
+	 * タスクを実行したトータルカウント数を取得
+	 *
+	 * @version 0.9.0
+	 * @since 0.9.0
+	 *
+	 */
+	public getCount (): number {
+		return this._iterator;
 	}
 
-	public act (sequence: Sequence, sequenceIndex: number, value: any): any {
-		var result: any = this._func.call(sequence, sequenceIndex, value);
-		this.status = TaskState.done;
+
+}
+
+/**
+ * タスクのクラス
+ *
+ * @private
+ * @version 0.9.0
+ * @since 0.4.0
+ *
+ */
+class Task {
+
+	/**
+	 * 紐づくシーケンサ
+	 *
+	 * @version 0.9.0
+	 * @since 0.9.0
+	 *
+	 */
+	private _sequencer: Sequence;
+
+	/**
+	 * ___
+	 * TODO: ネイティブのPromiseを使う
+	 *
+	 * @version 0.4.0
+	 * @since 0.4.0
+	 *
+	*/
+	private _func: Function;
+
+	/**
+	 * コンストラクタ
+	 *
+	 * @version 0.9.0
+	 * @since 0.4.0
+	 *
+	 */
+	constructor (func: Function, sequencer: Sequence) {
+		this._func = func;
+		this._sequencer = sequencer
+	}
+
+	/**
+	 * タスクの実行
+	 *
+	 * @version 0.9.0
+	 * @since 0.4.0
+	 * @param value タスクに渡すデータ
+	 * @return 実行したタスクの戻り値
+	 *
+	 */
+	public act (value: any): any {
+		let result: any = this._func.call(this._sequencer, this._sequencer.getCount(), value);
 		return result;
 	}
 
-}
-
-enum TaskState {
-	done,
-	yet
-}
-
-function isJQueryPromiseLikeObject (object: any): boolean {
-	var props: string[] = [
-		'always',
-		'done',
-		'fail',
-		'pipe',
-		'progress',
-		'promise',
-		'state',
-		'then'
-	];
-	if (object instanceof jQuery) {
-		return !!object.promise;
-	} else {
-		while (props.length) {
-			if (!(props.shift() in Object(object))) {
-				return false;
-			}
-		}
-		return true;
-	}
 }
 
 export = Sequence;
